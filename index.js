e881cff04b71816f56f0159164df5202cea54552
Yining Wang <yining.wang@utexas.edu>
2019-12-09 15:15:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, gift) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`);
+    }
+    return messages;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--)
+    }
+}
